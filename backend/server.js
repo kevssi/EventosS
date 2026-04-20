1296e315dd60d04559580ba19e1cffc34e5b111d
Kevin Uriel <talamantes.silva@icloud.com>
2026-04-20 09:32:48 -0700

    fix: agregar PATCH a metodos CORS permitidos

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(cors({
     'http://localhost:5000',
     'http://localhost:3000'
   ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
